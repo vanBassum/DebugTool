7bb4ff7fbb3747b5b6057c3091e1fc9e0b932247
Bas Visscher <bas.visscher24@gmail.com>
2021-02-12 14:54:27 +0100

    test with funcgen display

--- DebugTool/Form1.Designer.cs
+++ DebugTool/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -38,13 +37,14 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -54,7 +54,7 @@
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(126, 100);
+            this.panel1.Size = new System.Drawing.Size(148, 100);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -70,7 +70,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(3, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.Size = new System.Drawing.Size(142, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Connect";
             this.button1.UseVisualStyleBackColor = true;
@@ -80,9 +80,9 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(3, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "127.0.0.1:1000";
+            this.textBox1.Text = "192.168.11.50:32770";
             // 
             // listBox1
             // 
@@ -106,74 +106,80 @@
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Location = new System.Drawing.Point(178, 41);
+            this.tabControl1.Location = new System.Drawing.Point(166, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(400, 195);
+            this.tabControl1.Size = new System.Drawing.Size(482, 457);
             this.tabControl1.TabIndex = 4;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.listBox2);
             this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.pictureBox1);
             this.tabPage1.Controls.Add(this.button3);
-            this.tabPage1.Controls.Add(this.textBox2);
-            this.tabPage1.Controls.Add(this.checkBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(392, 169);
+            this.tabPage1.Size = new System.Drawing.Size(474, 431);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "FunctionGenerator";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(122, 61);
+            this.button3.Location = new System.Drawing.Point(35, 6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Set";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "FillScreen";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.SetFreq);
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // textBox2
+            // pictureBox1
             // 
-            this.textBox2.Location = new System.Drawing.Point(16, 61);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(42, 31);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(44, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Led";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.SetLED);
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(23, 23);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(311, 114);
+            this.button4.Location = new System.Drawing.Point(6, 136);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Set";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Exec";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 35);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(357, 95);
+            this.listBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 335);
+            this.ClientSize = new System.Drawing.Size(919, 481);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
@@ -186,6 +192,7 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -200,11 +207,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
---
+++ DebugTool/Form1.cs
@@ -0,0 +1,200 @@
+﻿using FRMLib;
+using STDLib.Ethernet;
+using STDLib.JBVProtocol;
+using STDLib.JBVProtocol.Devices;
+using STDLib.JBVProtocol.DSP50xx;
+using STDLib.JBVProtocol.FunctionGenerator;
+using STDLib.Misc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DebugTool
+{
+    public partial class Form1 : Form
+    {
+        JBVClient client = new JBVClient(SoftwareID.DebugTool);
+        TcpSocketClient socket = new TcpSocketClient();
+        BindingList<Device> devices = new BindingList<Device>();
+        Device selectedDevice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client.SetConnection(new TCPConnection(socket));
+            client.LeaseRecieved += Client_LeaseRecieved;
+            Device.Init(client);
+            Device.OnDeviceFound += Device_OnDeviceFound;
+            listBox1.DataSource = devices;
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            foreach (string file in Directory.GetFiles(path, "*.lst"))
+            {
+                listBox2.Items.Add(Path.GetFileName(file));
+            }
+
+
+        }
+
+        private void Client_LeaseRecieved(object sender, Lease e)
+        {
+            label1.InvokeIfRequired(() => label1.Text = e.ToString());
+        }
+
+        private void Device_OnDeviceFound(object sender, Device e)
+        {
+            listBox1.InvokeIfRequired(()=> devices.Add(e));
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = !await socket.ConnectAsync(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            devices.Clear();
+            Device.SearchDevices();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is Device dev)
+                selectedDevice = dev;
+
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (selectedDevice is FunctionGenerator dev)
+            {
+                bool oke = await dev.FillScreen(pictureBox1.BackColor);
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                Color c = cd.Color;
+                pictureBox1.BackColor = c;
+            }
+        }
+
+
+        int GetInt(string line, string pattern, int def)
+        {
+            int val = def;
+            Match m = Regex.Match(line, pattern);
+            if (m.Success)
+                val = int.Parse(m.Groups[1].Value);
+            return val;
+        }
+
+
+
+        async Task<bool> ParseLine(string line)
+        {
+            if (selectedDevice is FunctionGenerator dev)
+            {
+                if (line.StartsWith("BGND"))
+                {
+                    Color c = Color.Black;
+                    int r = GetInt(line, @"R:(\d+)", c.R);
+                    int g = GetInt(line, @"G:(\d+)", c.G);
+                    int b = GetInt(line, @"B:(\d+)", c.B);
+                    return await dev.FillScreen(Color.FromArgb(r, g, b));
+                }
+                else if (line.StartsWith("DELAY"))
+                {
+                    int delay = GetInt(line, @"T:(\d+)", 1000);
+                    System.Threading.Thread.Sleep(delay);
+                    return true;
+                }
+                else if (line.StartsWith("LINE"))
+                {
+                    Color c = Color.Red;
+                    int r = GetInt(line, @"R:(\d+)", c.R);
+                    int g = GetInt(line, @"G:(\d+)", c.G);
+                    int b = GetInt(line, @"B:(\d+)", c.B);
+                    UInt16 x1 = (UInt16)GetInt(line, @"X1:(\d+)", 0);
+                    UInt16 x2 = (UInt16)GetInt(line, @"X2:(\d+)", 0);
+                    UInt16 y1 = (UInt16)GetInt(line, @"Y1:(\d+)", 0);
+                    UInt16 y2 = (UInt16)GetInt(line, @"Y2:(\d+)", 0);
+
+                    return await dev.DrawLine(x1, y1, x2, y2, Color.FromArgb(r, g, b));
+                }
+            }
+            return false;
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+
+            if (listBox2.SelectedItem is string file)
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                string[] lines;
+
+                using (StreamReader rdr = new StreamReader(Path.Combine(path, file)))
+                    lines = rdr.ReadToEnd().Split('\n');
+
+
+
+                int i = 0;
+
+                for(i = 0; i<lines.Length; i++)
+                {
+                    
+                    string line = lines[i].Trim('\r');
+
+                    label2.Text = $"{i} - {line}";
+
+                    if (line.StartsWith("GOTO"))
+                    {
+                        string label = line.Substring(4).Trim('\t').Trim(' ') + ":";
+
+                        int ind = 0;
+                        for (ind = 0; ind < lines.Length; ind++)
+                        {
+                            if (lines[ind].StartsWith(label))
+                            {
+                                i = ind;   //Next round i will be increased by 1.
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        bool oke = await ParseLine(line);
+                        if (oke)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+            button4.Enabled = true;
+        }
+    }
+}
